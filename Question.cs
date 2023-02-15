ccc73e95c9afe7e6a8a5659b71237c55702a878c
eladm <eladmaayan@gmail.com>
2023-02-15 12:15:40 +0200

    Add project files.

---
+++ Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telhai.CS.FinalProject
+{
+    public class Question
+    {
+        public string content;
+        public int correct;
+        List<string> answers;
+
+    }
+}
---
+++ exam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telhai.CS.FinalProject
+{
+    public class exam
+    {
+        public string examName { get; set; }
+        public string id { get; set; }
+        public DateTime date { get; set; }
+        public string TeacherName { get; set; }
+        public DateTime BTime { get; set; }
+        public float duration { get; set; }
+        public bool isRandom { get; set; }
+        public List<Question> questions { get; set; };
+
+        public exam(string exName)
+        {
+            this.examName = exName;
+            this.id = Guid.NewGuid().ToString();
+        }
+    }
+}
